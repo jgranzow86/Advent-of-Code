1e34318ec2fc20401f3f3ffc9d875ee3b3225baf
Jason Granzow <jason.granzow@gmail.com>
2023-12-01 09:26:22 -0600

    Day 1, part 1

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Advent-of-Code-2023
-These are my submissions for the 2023 advent of code.
+# Advent-of-Code
+These are my solutions for the advent of code. Each year is it's own branch.
--- src/day01/day01.rs
+++ src/day01/day01.rs
@@ -1,5 +1,54 @@
+use std::str::FromStr;
+
 fn main() {
     let input = include_str!("input.txt");
 
-    println!("{input}");
+    let answer: u32 = part1(input);
+    println!("{answer}");
+}
+
+fn part1(input: &str) -> u32 {
+    let mut sum: u32 = 0;
+
+    for line in input.lines() {
+        let mut numbers: String = String::new();
+        for ch in line.chars() {
+            if ch.is_digit(10) {
+                numbers.push(ch);
+            }
+        }
+        
+        if numbers.len() > 2 {
+            let mut chars = numbers.chars();
+            let x = chars.next().unwrap();
+            let y = chars.next_back().unwrap();
+            numbers = x.to_string();
+            numbers.push(y);
+        }
+
+        if numbers.len() < 2 {
+            numbers.push(numbers.clone().chars().next().unwrap());
+        }
+
+        sum += numbers.parse::<u32>().unwrap();
+    }
+
+    sum
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test1() {
+        let sample: &str = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet";
+
+        let sum: u32 = part1(sample);
+        assert_eq!(sum, 142);
+    }
+
+    #[test]
+    fn test2() {
+        let sample: &str = "two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen"
+    }
 }
