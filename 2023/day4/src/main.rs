55eba030dd56c12a3aef0011f8e0defa69a05514
Jason Granzow <jason.granzow@gmail.com>
2023-12-05 00:49:06 -0600

    Day 4 part 2

--- 2023/day4/src/main.rs
+++ 2023/day4/src/main.rs
@@ -1,6 +1,3 @@
-use std::collections::HashSet;
-// use std::iter::FromIterator;
-
 use nom::{IResult,
     sequence::tuple,
     bytes::complete::tag,
@@ -11,23 +8,26 @@ use nom::{IResult,
 
 fn main() {
     let input = include_str!("input.txt");
-
-
-    let part1_answer = part1(input);
-    println!("Part 1: {part1_answer}");
-}
-
-fn part1(input: &str) -> u64 {
-    let mut total_score = 0;
-    let cards: Vec<ScratchCard> = input
+    let mut cards: Vec<ScratchCard> = input
         .lines()
         .into_iter()
         .map(|line| ScratchCard::new_from_str(line))
         .collect();
 
+
+    let part1_answer = part1(&cards);
+    println!("Part 1: {part1_answer}");
+
+    let part2_answer = part2(&mut cards);
+    println!("Part 2: {part2_answer}")
+}
+
+fn part1(cards: &[ScratchCard]) -> u64 {
+    let mut total_score = 0;
+
     for card in cards {
         let mut win_count = 0;
-        for drawn_number in card.drawn_numbers {
+        for drawn_number in &card.drawn_numbers {
             if card.winning_numbers.contains(&drawn_number) {
                 win_count += 1;
             }
@@ -39,10 +39,31 @@ fn part1(input: &str) -> u64 {
     total_score
 }
 
+fn part2(cards: &mut[ScratchCard]) -> u64 {
+    let mut total_card_count = 0;
+
+    for index in 0..cards.len() {
+        let mut win_count = 0;
+        for drawn_number_index in 0..cards[index].drawn_numbers.len() {
+            if cards[index].winning_numbers.contains(&cards[index].drawn_numbers[drawn_number_index]) {
+                win_count += 1;
+                let next_index = index + win_count;
+                cards[next_index].card_count += cards[index].card_count;
+            }
+        }
+    }
+
+    for card in cards {
+        total_card_count += card.card_count;
+    }
+    total_card_count
+}
+
 struct ScratchCard {
     id: u64,
     winning_numbers: Vec<u64>,
     drawn_numbers: Vec<u64>,
+    card_count: u64,
 }
 
 impl ScratchCard {
@@ -50,7 +71,8 @@ impl ScratchCard {
         let (input, id) = parse_card_id(input).finish().unwrap();
         let (input, winning_numbers) = parse_winning_numbers(input).finish().unwrap();
         let (_, drawn_numbers) = parse_number_list(input).finish().unwrap();
-        let scratch_card = ScratchCard{id, winning_numbers, drawn_numbers};
+        let card_count = 1;
+        let scratch_card = ScratchCard{id, winning_numbers, drawn_numbers, card_count};
         scratch_card
     }
 }
@@ -113,8 +135,33 @@ Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
 
-        let sum = part1(&sample);
+        let cards: Vec<ScratchCard> = sample
+            .lines()
+            .into_iter()
+            .map(|line| ScratchCard::new_from_str(line))
+            .collect();
+
+        let sum = part1(&cards);
         assert_eq!(sum, 13);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let sample = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+
+        let mut cards: Vec<ScratchCard> = sample
+        .lines()
+        .into_iter()
+        .map(|line| ScratchCard::new_from_str(line))
+        .collect();
+
+        let sum = part2(&mut cards);
+        assert_eq!(sum, 30);
     }
 
     #[test]
