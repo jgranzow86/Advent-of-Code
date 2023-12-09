54b05b9fe2a296e89240b635538032f7f8e10b2d
Jason Granzow <jason.granzow@gmail.com>
2023-12-08 23:00:03 -0600

    More dynamic

--- 2023/day8/src/main.rs
+++ 2023/day8/src/main.rs
@@ -1,18 +1,20 @@
 use std::{
     fs,
     str::FromStr,
-    time::Instant,
+    time::Instant, ops::Deref,
 };
 use nom::{
     Finish,
     IResult,
     sequence::tuple,
     character::complete::{
-        alpha1,
         space1,
-        anychar
+        anychar,
+        alphanumeric1
     }
 };
+use derivative::Derivative;
+use num::integer::lcm;
 
 fn main() {
     let input= fs::read_to_string("input.txt").expect("Error reading file");
@@ -64,38 +66,88 @@ fn part1(input: &str) -> u64 {
     step_count
 }
 
-fn part2(input: &String) -> u64 {
-    
-    0
+fn part2(input: &str) -> u64 {
+    let directions = input.lines().nth(0).unwrap();
+    let mut nodes = Vec::new();
+    let mut step_count =  0;
+    let mut starting_nodes = Vec::new();
+    let mut step_counts = Vec::new();
+
+
+    for line in input.lines().skip(2) {
+        let (_consumed_input, (source, left, right)) = parse_nodes(line).finish().unwrap();
+        let source = source.to_string();
+        let left = left.to_string();
+        let right = right.to_string();
+        let zero_step = 0;
+        nodes.push(Node { source, left, right, step_count: zero_step });
+    }
+
+    for node in nodes.as_slice() {
+        if node.source.chars().last() == Some('A') {
+            starting_nodes.push(node);
+        }
+    }
+
+    for start_node in starting_nodes {
+        let mut current_node: Node = start_node.clone();
+        step_count = 0;
+        'outer: loop {
+            for direction in directions.chars() {
+                current_node = nodes.iter().find(|node| {
+                    match direction {
+                        'L' => current_node.left == node.source,
+                        'R' => current_node.right == node.source,
+                        _ => panic!("This shouldn't happen!!!"),
+                    }
+                }).unwrap().clone();
+
+                step_count += 1;
+                if current_node.source.chars().last() == Some('Z') {
+                    break 'outer;
+                }
+            }
+        }
+        step_counts.push(step_count);
+    }
+    step_counts.iter().fold(1, |acc, &x| lcm(acc, x))
 }
 
+#[derive(Derivative)]
 #[derive(Clone, Default)]
 struct Node {
     source: String,
     left: String,
     right: String,
+    #[derivative(Default(value = "0"))]
+    step_count: u64,
 }
+
+// #[derive(Clone)]
+// struct Starts {
+//     current: Node,
+//     step_count: u64,
+// }
 
 impl Node {
     fn new(source: &str, left: &str, right: &str) -> Node {
         let source = String::from_str(source).unwrap();
         let left = String::from_str(left).unwrap();
         let right = String::from_str(right).unwrap();
-        Node { source, left, right }
+        let step_count = 0;
+        Node { source, left, right, step_count }
     }
 }
 
 fn parse_nodes(input: &str) -> IResult<&str, (&str, &str, &str)> {
     let (input, (source, _, _, _, _, left, _, _, right, _)) = 
-        tuple((alpha1, space1, anychar, space1, anychar, alpha1, anychar, space1, alpha1, anychar))(input)?;
+        tuple((alphanumeric1, space1, anychar, space1, anychar, alphanumeric1, anychar, space1, alphanumeric1, anychar))(input)?;
 
     Ok((input, (source, left, right)))
 }
 
 #[cfg(test)]
 mod tests {
-    use nom::Finish;
-
     use super::*;
     #[test]
     fn test_part_1() {
@@ -108,10 +160,19 @@ mod tests {
 
     #[test]
     fn test_part_2() {
-        let input = fs::read_to_string("sample.txt").expect("Error reading file");
+        let input = "LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)";
 
         let answer = part2(&input);
-        assert_eq!(answer, 30);
+        assert_eq!(answer, 6);
     }
 
     #[test]
@@ -122,7 +183,11 @@ mod tests {
 
         for line in input.lines().skip(2) {
             let (_consumed_input, (source, left, right)) = parse_nodes(line).finish().unwrap();
-            nodes.push(Node::new(source, left, right));
+            let source = source.to_string();
+            let left = left.to_string();
+            let right = right.to_string();
+            let step_count = 0;
+            nodes.push(Node { source, left, right, step_count });
         }
 
         let mut node = nodes.iter();
